233cbf1641b3a2fe430ca8b13668306f99c0d38b
Vishwas <vishwas.g5@gmail.com>
2025-03-08 13:51:17 +0530

    Tab Navigation

--- TabNavigationApp/app/_layout.tsx
+++ TabNavigationApp/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
