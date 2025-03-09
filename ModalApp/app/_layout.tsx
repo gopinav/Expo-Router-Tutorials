830b4eff6385a3e3da1b11c024c7a84db00674fe
Vishwas <vishwas.g5@gmail.com>
2025-03-09 19:57:46 +0530

    Modals

--- ModalApp/app/_layout.tsx
+++ ModalApp/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen
+        name="modal"
+        options={{ title: "Modal", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="webmodal"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
--- ModalApp/app/index.tsx
+++ ModalApp/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/modal">Open Modal</Link>
+      <Link href="/webmodal">Open Web Modal</Link>
     </View>
   );
 }
